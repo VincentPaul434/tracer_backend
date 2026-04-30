7976fc74cd046c21db5190c59321a0aa8c6ca466
VincentPaul434 <shadowvator@gmail.com>
2026-04-30 11:49:51 +0800

    Update IdUploadResponse.java

--- src/main/java/cit/nurse/tracer/submission/dto/IdUploadResponse.java
+++ src/main/java/cit/nurse/tracer/submission/dto/IdUploadResponse.java
@@ -1,9 +1,15 @@
 package cit.nurse.tracer.submission.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record IdUploadResponse(
         String idImageUrl,
         String publicId,
         String originalFilename,
         long bytes
 ) {
+        @JsonProperty("url")
+        public String url() {
+                return idImageUrl;
+        }
 }
